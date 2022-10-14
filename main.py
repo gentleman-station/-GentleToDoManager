b47f9e2e685e178f75cb8648e8c12b244c261bcc
gentleman-station <station.gentleman@gmail.com>
2022-10-15 04:32:07 +0545

    Added command-line flags

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
 from frontend import index
+from fire import Fire
 import flet
 
 
-def run():
-    flet.app(target=index)
+def run(execute: bool = True, web: bool = False):
+    """Execute #GentleToDoManager
+
+    Args:
+        execute (bool, optional): Weather to execute this application. (Default: True)
+        web (bool, optional): Use web version of this application while executing it. (Default: False)
+    """
+    if execute:
+        if web:
+            flet.app(target=index, view=flet.WEB_BROWSER)
+        else:
+            flet.app(target=index)
 
 
 if __name__ == '__main__':
-    run()
+    Fire(run)
